7b2a6c96564fd6b37ce3839808d0199e7fca2289
DussaUday <dussauday469@gmail.com>
2025-06-14 21:24:29 +0530

    portfolio

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 connectDB();
 
 // API Routes
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/certificates', certificateRoutes);
 app.use('/api/skills', skillRoutes);
